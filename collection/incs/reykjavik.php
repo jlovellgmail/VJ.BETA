545cc40e032ec2521e8ba0f951bc3123d516c5f8
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2016-12-12 15:20:23 -0800

    remove the top row with collection description from the collection base page and into the Reykjavik file

--- collection/incs/reykjavik.php
+++ collection/incs/reykjavik.php
@@ -1,19 +1,15 @@
+<div class="topRow">
+    <div class="headline">
+        <div class="part1">Reykjavik</div><div class="part2">Collection</div>
+    </div>
+    <div class="text">
+        Reykjavik is about beauty and raw strength. Its volcanic origin belies a region still in formation. This city exhibits a resolve to honor the past and embrace the future. Our selection of materials and original bronze designs are inspired by the unique independence of this remarkable city.
+    </div>
+</div>
+
 <div id="lineProducts" class="">
     <div class="bgWrapper">
         <div class="widthWrapper">
-
-	        <div class='rel block marBottomR1'>
-	            <div class="col lg-nine leftCol mTextCenterDLeft">
-	                <div class="tableWrapper h100p" style='padding-top: 0;'>
-	                    <div class="cellWrapper">
-	                        <span class="ital size5">Reykjavik is about beauty and raw strength.  It’s volcanic origin belies a region still in formation. This city exhibits a resolve to honor the past and embrace the future. Our selection of materials and original bronze designs are inspired by the unique independence of this remarkable&nbsp;city.</span>
-	                    </div>
-	                </div>
-	            </div><!-- 
-	         --><div class="col lg-three rightCol h100p collection-right-image">
-	         		<img src="/img/collection/rekjavik-hardware2.jpg" class="collectionHeroImg" style="vertical-align: middle; width: 200px;" />
-	     		</div>
-	 		</div>
 
             <!-- Collection grid probably goes here -->
             <?php 
--- collection/index.php
+++ collection/index.php
@@ -88,17 +88,6 @@ $CollID = $CollObj->getVar("CID");
             <div class="widthWrapper">
 
 
-                <div class="topRow">
-                    <div class="headline">
-                        <div class="part1">Reykjavik</div><div class="part2">Collection</div>
-                    </div>
-                    <div class="text">
-                        Reykjavik is about beauty and raw strength. Its volcanic origin belies a region still in formation. This city exhibits a resolve to honor the past and embrace the future. Our selection of materials and original bronze designs are inspired by the unique independence of this remarkable city.
-                    </div>
-                </div>
-
-
-
                 <!-- lineProducts loaded content via js goes here -->
                 <?php include $_SERVER['DOCUMENT_ROOT'] . '/collection/incs/' . $collection . '.php'; ?>
 
