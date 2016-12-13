5ab9140230657339b5327e1e4bdaca5ef79961af
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2016-12-13 11:17:30 -0800

    fix About page layout

--- about.php
+++ about.php
@@ -7,171 +7,163 @@
 	<title>About Virgil James | Bags, Luxury Handbags & Authentic Handbags</title>
 	<meta name="keywords" content="Handbags, bags, bag, Luxury bags, Authentic bags, Designer handbags, Virgil James"/>
 	<meta name="description" content="With headquarters in Los Angeles, California, Virgil James is the place to shop for luxury bags and accessories."/>
-    
+
+    <link rel="stylesheet" href="/css/bootstrap-grid.css" />
 	<?php include '/incs/head-links.php'; ?>
 	<link rel="stylesheet" href="/css/index.css">
 	<link rel="stylesheet" href="/css/about.css">
 </head>
 <body>
-<div class="sdWrapper">
-<div class="sdContent">
 
 <?php include '/incs/nav.php'; ?>
 
-<div class="landing-hero-wrapper h100vh">
-    <div class="aspect-dummy-43"></div>
-    <div class="aspect-img aspect-img-hero about-hero">
-        <div class="widthWrapper h100p">
-            <div class="tableWrapper h100p">
-                <div class="cellWrapper">
-                	<div class='xs-zero lg-six'></div><div class='xs-twelve lg-six textLeft hero-text-block'>
-                        <span class="product-message-title-topline size45">getting</span>
-                        <h1 class="product-message-title-bottomline caps size2 fw-400">Started</h1>
-                        <p class='fw-300 white'>Our original goal was to build exactly what we wanted to use and own, without compromise, with the belief that others would value this accomplishment and want the same.  Sure, it took much longer than planned (OK, three years longer) and cost more (yes, a lot more!) but we now make the exceptional products we originally&nbsp;envisioned.</p>
-                	</div>
+<div class="aboutPage bg1">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-xs-12 noPadding">
+                <div class="landingFrame">
+                    <div class="mainFrame">
+                        <div class="textBlockMain">
+                            <div class="blackGlow">
+                                <img src="/preorder/images/black_blur.png" />
+                            </div>
+                            <div class="headline">
+                                Getting Started
+                            </div>
+                            <div class="copy">
+                                Our original goal was to build exactly what we wanted to use and own, without compromise, with the belief that others would value this accomplishment and want the same. Sure, it took much longer than planned (OK, three years longer) and cost more (yes, a lot more!) but we now make the exceptional products we originally envisioned.
+                            </div>
+                        </div>
+                    </div>
+                    <img class="downArrow" id="scrollDownArrow" src="/img/arrow_down.svg">
                 </div>
             </div>
         </div>
     </div>
-    <div class='scroll-arrow-indicator'><i class='icon-angle-down'></i></div>
+    <div class=belowLandingFrame>
+        <div class="container">
+            <!-- --------------------------------------------------- -->
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="image" id="designPrinciples">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="sectionHeading">
+                        Design Principles
+                    </div>
+                    <div class="copy">
+                        Like the creation of art, we're driven to design and build extraordinary bags and accessories that express what's important to us. Our design approach is called Engineered Luxury. It starts by blending a classically elegant, timeless look with real-life modern usability. It ends by using the best materials and craftsmen to create something that's authentic and unique.
+                    </div>
+                </div>
+            </div>
+            <!-- --------------------------------------------------- -->
+            <div class="row vjRowTopMargin">
+                <div class="col-lg-6">
+                    <div class="image" id="whatMakesUsUnique">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="sectionHeading">
+                        What Makes us Unique
+                    </div>
+                    <div class="copy">
+                        Admittedly, the luxury accessories business is glutted with wannabes and successful big names. At some point, almost all of them are forced to compromise, to maintain a price, or to sell for more. Virgil James hasn't compromised on anything. We sell limited edition products, directly to customers, for a fair price that reflects the product's quality and authenticity.
+                    </div>
+                </div>
+            </div>
+            <!-- --------------------------------------------------- -->
+            <div class="row vjRowTopMargin">
+                <div class="col-lg-6">
+                    <div class="image" id="productCollections">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="sectionHeading">
+                        Product Collections
+                    </div>
+                    <div class="copy">
+                        Product collections are guided by the creativity we see and experience through travel, culture, and art. Our limited-edition CITYLINE collections are inspired by great cities. Our MODERNE Collections showcase a contemporary use of classic materials. Collections and styles are introduced at a time and pace that allows us to maintain uncompromised authenticity.
+                    </div>
+                </div>
+            </div>
+            <!-- --------------------------------------------------- -->
+            <div class="row vjRowTopMargin">
+                <div class="col-lg-6">
+                    <div class="image" id="makingTheBest">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="sectionHeading">
+                        Making the Best
+                    </div>
+                    <div class="copy">
+                        It's no secret - exceptional products require great materials and expert craftsman. We use both. We scoured the luxury world for the best components and, when that wasn't enough, we had them made. This took time. We also employed craftsmen who shared our view of uncompromising quality. This took more time but it's necessary if you want to make the best.
+                    </div>
+                </div>
+            </div>
+            <!-- --------------------------------------------------- -->
+            <div class="row vjRowTopMargin">
+                <div class="col-lg-6">
+                    <div class="image" id="qualityMaterials">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="sectionHeading">
+                        Quality Materials
+                    </div>
+                    <div class="copy">
+                        Our leathers come from, arguably, the best tannery in Italy. The same is true for all of our woven materials, including canvas. Our Cashmere felt - probably the only material of its kind - is handmade in Mongolia. The metal components and zippers are easily among the best in the world. Our bronze hardware is handmade like the art it really is, and the carabiners are a story onto themselves.
+                    </div>
+                </div>
+            </div>
+            <!-- --------------------------------------------------- -->
+            <div class="row vjRowTopMargin">
+                <div class="col-lg-6">
+                    <div class="image" id="theTeam">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="sectionHeading">
+                        The Team
+                    </div>
+                    <div class="copy">
+                        Companies are a natural expression of their people. Our founders and team are probably a lot like you. They travel frequently and they value things done well, with style. They've worked in creative and professional fields and lead interesting lives. They are humble yet driven to accomplish and produce exceptional outcomes. They work to satisfy people who appreciate excellence.
+                    </div>
+                </div>
+            </div>
+            <!-- --------------------------------------------------- -->
+            <div class="row vjRowTopMargin">
+                <div class="col-lg-6">
+                    <div class="image" id="buyingFromVirgilJames">
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="sectionHeading">
+                        Buying From Virgil James
+                    </div>
+                    <div class="copy">
+                        We use and support a direct-sales approach. Customers can purchase our products directly or through our Ambassadors who will meet customers individually or with a group at a scheduled event. Of course, eliminating the retailer saves you money, but the primary reason we do this is to ensure that each customer's experience (before, during, and after a purchase) is as good as the product.
+                    </div>
+                </div>
+            </div>
+            <!-- --------------------------------------------------- -->
+            <div class="row vjRowTopMargin">
+                <div class="col-lg-6">
+                    <div class="image" id="extraImage1">
+                    </div>
+                </div>
+            </div>
+            <!-- --------------------------------------------------- -->
+        </div>
+    </div>
 </div>
 
-<div class='scroll-down-to'></div>
-<div class='widthWrapper about-body marBottom60'>
-
-    <div class='block-1'>
-        <div class='block-1-row-1'>
-        	<div class='principles-text'>
-                <span class="message-title-topline-black size45">design</span>
-                <h1 class="message-title-black caps size2 fw-400">Principles</h1>
-                <p class='rel block'>Like the creation of art, we’re driven to design and build extraordinary bags and accessories that express what’s important to us.  Our design approach is called Engineered Luxury.  It starts by blending a classically elegant, timeless look with real-life modern usability.  It ends by using the best materials and craftsmen to create something that’s authentic&nbsp;and&nbsp;unique.</p>
-            </div><!-- 
-         --><div class='bag-handle-wrapper'>
-                <div class='bag-handle-aspect'></div>
-                <div class='bag-handle-content'></div>
-            </div>
-        </div>
-        <div class='block-1-row-2'>
-            <div class='leather-folds-wrapper'>
-                <div class='leather-folds-aspect'></div>
-                <div class='leather-folds-content'></div>
-            </div><!-- 
-         --><div class='unique-text textLeft'>
-                <span class="message-title-topline-black size45">what makes us</span>
-                <h1 class="message-title-black caps size2 fw-400">Unique</h1>
-                <p>Admittedly, the luxury accessories business is glutted with wannabes and successful big names.  At some point, almost all of them are forced to compromise, to maintain a price, or to sell for more.  Virgil James hasn’t compromised on anything.  We sell limited edition products, directly to customers, for a fair price that reflects the product’s quality&nbsp;and&nbsp;authenticity.</p>
-            </div>
-        </div>
-        <div class='center-leather'></div>
-    </div>
-
-    <div class='block-2'>
-        <div class='route66-wrapper'>
-            <div class='route66-aspect'></div>
-            <div class='route66-content'></div>
-        </div>
-        <div class='collections-text textLeft'>
-            <span class="message-title-topline-black size45 white">product</span>
-            <h1 class="product-message-title-bottomline caps size2 fw-400">Collections</h1>
-            <p>Product collections are guided by the creativity we see and experience through travel, culture, and art.  Our limited-edition CITYLINE collections are inspired by great cities.  Our MODERNE Collections showcase a contemporary use of classic materials.  Collections and styles are introduced at a time and pace that allows us to maintain uncompromised&nbsp;authenticity.</p>
-        </div>
-    </div>
-
-    <div class='block-3'>
-        <div class='block-3-row-1'>
-            <div class='villa-wrapper'>
-                <div class='villa-aspect'></div>
-                <div class='villa-content'></div>
-            </div><!-- 
-         --><div class='limited-title textRight'>
-                <span class="message-title-topline-black size45">limited edition</span>
-                <h1 class="message-title-black caps size2 fw-400">CityLine</h1>
-                <span class="message-title-topline-black size45 rel block marBottom15">reykjavik</span>
-                <span class="message-title-topline-black size45 rel block marBottom15">santa fe</span>
-                <span class="message-title-topline-black size45 rel block marBottom15">buenos aires</span>
-            </div>
-        </div>
-        <div class='block-3-row-2'>
-            <div class='forever-title textLeft'>
-                <span class="message-title-topline-black size45">forever</span>
-                <h1 class="message-title-black caps size2 fw-400">Moderne</h1>
-                <span class="message-title-topline-black size45 rel block marBottom15">classic canvas</span>
-                <span class="message-title-topline-black size45 rel block marBottom15">cashmere felt</span>
-                <span class="message-title-topline-black size45 rel block">signature</span>
-            </div><!-- 
-         --><div class='iceland-wrapper'>
-                <div class='iceland-aspect'></div>
-                <div class='iceland-content'></div>
-            </div>
-        </div>
-        <div class='center-dress'></div>
-    </div>
-
-    <div class='block-4'>
-        <div class='makers-tools-wrapper'>
-            <div class='makers-tools-aspect'></div>
-            <div class='makers-tools-content'></div>
-        </div>
-    	<div class='best-text textLeft'>
-            <span class="message-title-topline-black size45 white">making</span>
-            <h1 class="product-message-title-bottomline caps size2 fw-400">The Best</h1>
-            <p class='fw-300'>It’s no secret - exceptional products require great materials and expert craftsman.  We use both.  We scoured the luxury world for the best components and, when that wasn’t enough, we had them made.  This took time.  We also employed craftsmen who shared our view of uncompromising quality.  This took more time but it’s necessary if you want to make&nbsp;the&nbsp;best.</p>
-        </div>
-    </div>
-
-    <div class='block-5'>
-        <div class='block-5-row-1'>
-            <div class='badge-wrapper'>
-                <div class='badge-aspect'></div>
-                <div class='badge-content'></div>
-            </div><!-- 
-    	 --><div class='materials-title textRight'>
-                <span class="message-title-topline-black size45">quality</span>
-                <h1 class="message-title-black caps size2 fw-400">Materials</h1>
-            </div>
-        </div>
-        <div class='block-5-row-2'>
-            <div class='materials-text textLeft'>
-                <p class='fw-300'>Our leathers come from, arguably, the best tannery in Italy.  The same is true for all of our woven materials, including canvas.  Our Cashmere felt - probably the only material of its kind - is handmade in Mongolia.  The metal components and zippers are easily among the best in the world.  Our bronze hardware is handmade like the art it really is, and the carabiners are a story onto&nbsp;themselves.</p>
-            </div><!-- 
-         --><div class='laborers-wrapper'>
-                <div class='laborers-aspect'></div>
-                <div class='laborers-content'></div>
-            </div>
-        </div>
-        <div class='center-folds'></div>
-    </div>
-
-    <div class='block-6'>
-        <div class='dining-wrapper'>
-            <div class='dining-aspect'></div>
-            <div class='dining-content'></div>
-        </div>
-    	<div class='team-text textLeft'>
-            <span class="message-title-topline-black size45 white">the</span>
-            <h1 class="product-message-title-bottomline caps size2 fw-400">Team</h1>
-            <p class='fw-300'>Companies are a natural expression of their people.  Our <button class='bio-modal-link' onclick="showModal('/incs/modals/modal-about-bio.php');"><u>founders</u></button> and team are probably a lot like you.  They travel frequently and they value things done well, with style.  They’ve worked in creative and professional fields and lead interesting lives.  They are humble yet driven to accomplish and produce exceptional outcomes.  They work to satisfy people who appreciate&nbsp;excellence.</p>
-        </div>
-    </div>
-
-    <div class='block-7'>
-    	<div class='buying-text'>
-            <span class="message-title-topline-black size45">buying from</span>
-            <h1 class="message-title-black caps size2 fw-400">Virgil James</h1>
-            <p class='fw-300'>We use and support a direct-sales approach.  Customers can purchase our products <a href='shop.php'><u>directly</u></a> or through our <a href='ambassadors.php'><u>Ambassadors</u></a> who will meet customers individually or with a group at a scheduled event.  Of course, eliminating the retailer saves you money, but the primary reason we do this is to ensure that each customer’s experience (before, during, and after a purchase) is as good as&nbsp;the&nbsp;product.</p>
-        </div>
-    </div>
-
-</div>
-
-</div>
 <?php include '/incs/footer.php'; ?>
 <?php include '/incs/footer-links.php'; ?>
 
-</div>
-
     <script>
-        $('.scroll-arrow-indicator').click(function () {
+        $('#scrollDownArrow').click(function () {
             $('html, body').animate({
                 scrollTop: ($('.scroll-down-to').offset().top)
             },750);
@@ -179,7 +171,7 @@
     </script>
 
     <script>
-        var target = $('.scroll-arrow-indicator');
+        var target = $('#scrollDownArrow');
         var targetHeight = $(window).height();
 
         $(document).scroll(function(e){
