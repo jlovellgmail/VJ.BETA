1f738ef59e980051f0e248740a8014b9b6bb27e2
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2016-12-27 07:00:07 -0800

    copy index_framed.php into index.php

--- index.php
+++ index.php
@@ -21,112 +21,127 @@ $seo_variable = "home";
         <?php include '/incs/nav.php'; ?>
 
 
-        <div class="preorderPage">
-            <div class="landingFrame">
-                <div class="mainFrame">
+
+
+        <div class="aboutPage landingPageFramed">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-xs-12 noPadding">
+                        <div class="landingFrame">
+                            <div class="imageContainer">
                     
 
-                    <div class="contentContainer">
-                        <!-- carousel -->
-                        <div class="imageContainer">
-                            <div class="image">
-                                
-                                <img id="carouselImage" class="active" src="/img/IMG_6948_tote_hero_whiter_bg_q7.jpg" />
-                                <img id="carouselImage2" style="opacity: 0;" src="/img/IMG_6948_tote_hero_whiter_bg_q7_test.jpg" />
 
-                            </div>
-                            <div class="controls">
-                                <div class="dot active"></div>
-                                <div class="dot"></div>
-                                <div class="dot"></div>
-                            </div>
-                        </div>
-                        <div class="textContainer">
-                            <div class="preorderText">
-                                <div class="headlineContainer">
-                                    <div class="headline">
-                                        <div class="part1 light">Engineered</div>
-                                        <div class="part2 heavy">Luxury</div>
+
+                                <div class="contentContainer">
+                                    <!-- carousel -->
+                                    <div class="carouselContainer">
+                                        <div class="carouselImageContainer">
+                                            
+                                            <img id="carouselImage" class="active" src="/img/IMG_6948 - tote hero - transparent for postcard bg.png" />
+                                            <img id="carouselImage2" style="opacity: 0;" src="/img/carousel image - Drawstring - transparent.png" />
+
+                                        </div>
+                                        <div class="controls">
+                                            <div class="dot active"></div>
+                                            <div class="dot"></div>
+                                            <div class="dot"></div>
+                                        </div>
+                                    </div>
+                                    <div class="textContainer">
+                                        <div class="preorderText">
+                                            <div class="headlineContainer">
+                                                <div class="headline">
+                                                    <div class="part1 light">Engineered</div>
+                                                    <div class="part2 heavy">Luxury</div>
+                                                </div>
+                                            </div>
+                                            <div class="copy">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales lorem nulla, non finibus lacus interdum eu. Nam et ligula efficitur, volutpat tortor sed, pulvinar leo. Vestibulum condimentum nisl augue, ut mollis nunc tempus vel. In sed felis tellus.
+                                            </div>
+
+                                            <a class="box" href="/shop/">
+                                                <div class="title">
+                                                    Shop Now
+                                                </div>
+                                            </a>
+
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="copy">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales lorem nulla, non finibus lacus interdum eu. Nam et ligula efficitur, volutpat tortor sed, pulvinar leo. Vestibulum condimentum nisl augue, ut mollis nunc tempus vel. In sed felis tellus.
+
+
+                                <!-- for carousel: preload images -->
+                                <div style="position:fixed; left:4000px; top:4000px; opacity: 0; ">
+                                    <img src="/img/IMG_6948 - tote hero - transparent for postcard bg.png">
+                                    <img src="/img/carousel image - Drawstring - transparent.png">
+                                    <img src="/img/carousel image - Clutch - transparent.png">
+
                                 </div>
-
-                                <a class="box" href="/shop/">
-                                    <div class="title">
-                                        Shop Now
-                                    </div>
-                                </a>
-
-                            </div>
-                        </div>
-                    </div>
-
-
-                    <!-- for carousel: preload images -->
-                    <div style="position:fixed; left:4000px; top:4000px; opacity: 0; ">
-                        <img src="/img/IMG_6948_tote_hero_whiter_bg_q7.jpg">
-                        <img src="/img/IMG_6948_tote_hero_whiter_bg_q7_test.jpg">
-                        <img src="/img/IMG_6948_tote_hero_whiter_bg_q7_test2.jpg">
-
-                    </div>
-                    <!-- for carousel: change images -->
-                    <script>
-                        var images = [
-                            "/img/IMG_6948_tote_hero_whiter_bg_q7.jpg"
-                            ,"/img/IMG_6948_tote_hero_whiter_bg_q7_test.jpg"
-                            ,"/img/IMG_6948_tote_hero_whiter_bg_q7_test2.jpg"
-                        ];
-                        var i=0;
-                        var initialTimeout = 8000;
-                        var timeout = 8000;
-                        var delay = 600;
-                        function change(){
-                            if($("#carouselImage").hasClass("active")){
-                                current = $("#carouselImage");
-                                next = $("#carouselImage2");
-                            }
-                            else {
-                                current = $("#carouselImage2");
-                                next = $("#carouselImage");
-                            }
-                            i = ++i % images.length;
-                            function animate(){
-                                var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-                                current.addClass("fadeout").one(animationEnd, function() {
-                                    $(this).removeClass("fadeout");
-                                    $(this).css("opacity", 0);
-                                });
-                                setTimeout(showNext, delay);
-                                function showNext(){
-                                    current.removeClass("active");
-                                    next.addClass("active");
-                                    next.attr("src", images[i]);
-                                    next.addClass("fadein").one(animationEnd, function() {
-                                        $(this).removeClass("fadein");
-                                        $(this).css("opacity", 1);
+                                <!-- for carousel: change images -->
+                                <script>
+                                    var images = [
+                                        "/img/IMG_6948 - tote hero - transparent for postcard bg.png"
+                                        ,"/img/carousel image - Drawstring - transparent.png"
+                                        ,"/img/carousel image - Clutch - transparent.png"
+                                    ];
+                                    var i=0;
+                                    var initialTimeout = 8000;
+                                    var timeout = 8000;
+                                    var delay = 600;
+                                    function change(){
+                                        if($("#carouselImage").hasClass("active")){
+                                            current = $("#carouselImage");
+                                            next = $("#carouselImage2");
+                                        }
+                                        else {
+                                            current = $("#carouselImage2");
+                                            next = $("#carouselImage");
+                                        }
+                                        i = ++i % images.length;
+                                        function animate(){
+                                            var animationEnd = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+                                            current.addClass("fadeout").one(animationEnd, function() {
+                                                $(this).removeClass("fadeout");
+                                                $(this).css("opacity", 0);
+                                            });
+                                            setTimeout(showNext, delay);
+                                            function showNext(){
+                                                current.removeClass("active");
+                                                next.addClass("active");
+                                                next.attr("src", images[i]);
+                                                next.addClass("fadein").one(animationEnd, function() {
+                                                    $(this).removeClass("fadein");
+                                                    $(this).css("opacity", 1);
+                                                });
+                                                $(".dot").removeClass("active");
+                                                $(".dot:nth-of-type("+ (i+1) +")").addClass("active");
+                                            }
+                                        }
+                                        animate();
+                                        setTimeout(change, timeout);
+                                    }
+                                    $(window).load(function(){
+                                        setTimeout(change, initialTimeout);
                                     });
-                                    $(".dot").removeClass("active");
-                                    $(".dot:nth-of-type("+ (i+1) +")").addClass("active");
-                                }
-                            }
-                            animate();
-                            setTimeout(change, timeout);
-                        }
-                        $(window).load(function(){
-                            setTimeout(change, initialTimeout);
-                        });
-                    </script>
+                                </script>
+
+                                <!--
+                                <div class="imageWrapper">
+                                </div>
+                                -->
 
 
-                    <div class="imageWrapper">
+
+
+                            </div>
+                        </div>
                     </div>
-
-
                 </div>
             </div>
         </div>
+
+
 
 
         <?php include '/incs/footer.php'; ?>
