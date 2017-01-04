137c911866fb3f0dbfb8ab17e5d3dd0c22ac56c9
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2017-01-03 17:29:25 -0800

    collections page: move new structure to live page, rename the old one so it matches the class that needs to be deleted with this

--- collections.php
+++ collections.php
@@ -21,49 +21,58 @@ include '/classes/Line.class.php';
     <?php include '/incs/nav.php'; ?>
 
 
+    <div class="collectionsPage-using-new-generic-170103 landingScreen-generic-170103">
+        
 
-    <div class="collectionsPage-using-generic-classes landingScreenMargins">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-xs-12 noPadding">
-                    <div class="landingFrame">
-                        <div class="imageContainer">
-                            <div class="contentContainer">
-                                <div class="preorderText">
-                                    <div class="titleContainer">
-                                        <div class="title">
-                                            <div class="part1">Cityline</div><div class="part2">Collections</div>
+
+
+                    <div class="landingHeight">
+                        <div class="landingWidth">
+                            <div class="imageFrame">
+                                <div class="table-cell">
+                                    <div class="contentContainer">
+                                        <div class="textBlockMain">
+                                            <div class="titleContainer">
+                                                <div class="title">
+                                                    <div class="part1">Cityline</div><div class="part2">Collections</div>
+                                                </div>
+                                            </div>
+                                            <div class="copy">
+                                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales lorem nulla, non finibus lacus interdum eu. Nam et ligula efficitur, volutpat tortor sed, pulvinar leo. Vestibulum condimentum nisl augue, ut mollis nunc tempus vel. In sed felis tellus.
+                                            </div>
+                                            <div class="boxes">
+                                                <a class="box" href="collection/index.php?line=city&col=reykjavik">
+                                                    <div class="title">
+                                                        Reykjavik
+                                                    </div>
+                                                    <div class="subtitle">
+                                                        Collection
+                                                    </div>
+                                                </a>
+                                                <div class="vline">
+                                                </div>
+                                                <a class="box" href="collection/index.php?line=city&col=santa-fe">
+                                                    <div class="title">
+                                                        Santa Fe
+                                                    </div>
+                                                    <div class="subtitle">
+                                                        Collection
+                                                    </div>
+                                                </a>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <div class="copy">
-                                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sodales lorem nulla, non finibus lacus interdum eu. Nam et ligula efficitur, volutpat tortor sed, pulvinar leo. Vestibulum condimentum nisl augue, ut mollis nunc tempus vel. In sed felis tellus.
-                                    </div>
-                                    <div class="boxes">
-                                        <a class="box" href="collection/index.php?line=city&col=reykjavik">
-                                            <div class="title">
-                                                Reykjavik
-                                            </div>
-                                            <div class="subtitle">
-                                                Collection
-                                            </div>
-                                        </a>
-                                        <div class="vline">
-                                        </div>
-                                        <a class="box" href="collection/index.php?line=city&col=santa-fe">
-                                            <div class="title">
-                                                Santa Fe
-                                            </div>
-                                            <div class="subtitle">
-                                                Collection
-                                            </div>
-                                        </a>
                                     </div>
                                 </div>
-                            </div>
+                            <img class="downArrow" id="scrollDownArrow" src="/img/arrow_down.svg">
+                            </div> 
                         </div>
-                        <img class="downArrow" id="scrollDownArrow" src="/img/arrow_down.svg">
                         <div class='scroll-down-to'></div>
                     </div>
+
+
                 </div>
             </div>
         </div>
@@ -78,6 +87,8 @@ include '/classes/Line.class.php';
                 </div>
             </div>
         </div>
+
+        
     </div>
 
 
