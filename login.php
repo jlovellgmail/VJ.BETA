8197ddfa089db56196da22f1743f11bf982f76e2
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2017-01-09 12:44:46 -0800

    clean up

--- login.php
+++ login.php
@@ -5,38 +5,22 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Login | Virgil James</title>
-        
         <?php include '/incs/head-links.php'; ?>
-        
         <link rel="stylesheet" href="/css/forms.css">
         <script src="/js/login_validation.js"></script>
     </head>
     <body>
         <?php include '/incs/nav.php'; ?>
-
-
-
         <div class="loginPage">
-
-
             <div class="formsContainer">
-
-
                 <div class="col left">
                     <?php  include '/incs/login.php'; ?>
                 </div>
                 <div class="col right">
                     <?php  include '/incs/signUp.php'; ?>
                 </div>
-
-
             </div>
-
-
         </div>
-
-
-
         <?php include '/incs/footer.php'; ?>
         <?php include '/incs/footer-links.php'; ?>
     </body>
