d46fb7f668074449d57a04d205ad28d0c356f47f
jlovellgmail <MJC@MJCs-MacBook-Pro.local>
2017-01-09 12:40:43 -0800

    login page: fix layout, apply classes from preorder that weren't being used - - also clean up code

--- login.php
+++ login.php
@@ -11,25 +11,33 @@
         <link rel="stylesheet" href="/css/forms.css">
         <script src="/js/login_validation.js"></script>
     </head>
-    <body class="loginBody">
-        <div class="sdWrapper">
-            <div class="sdContent">
-                <?php include '/incs/nav.php'; ?>
-                <div class="navPlaceholder"></div>
-                <div class="widthWrapper">
-                    <div class="tableWrapper tablevh100">
-                        <div class="cellWrapper">
-                            <div class="md-five lg-five leftCol">
-                                <?php  include '/incs/login.php'; ?>
-                            </div><div class="md-one lg-one"></div><div class="md-five lg-five rightCol">
-                                <?php  include '/incs/signUp.php'; ?>
-                            </div>
-                        </div>
-                    </div>
+    <body>
+        <?php include '/incs/nav.php'; ?>
+
+
+
+        <div class="loginPage">
+
+
+            <div class="formsContainer">
+
+
+                <div class="col left">
+                    <?php  include '/incs/login.php'; ?>
                 </div>
+                <div class="col right">
+                    <?php  include '/incs/signUp.php'; ?>
+                </div>
+
+
             </div>
-            <?php include '/incs/footer.php'; ?>
-            <?php include '/incs/footer-links.php'; ?>
+
+
         </div>
+
+
+
+        <?php include '/incs/footer.php'; ?>
+        <?php include '/incs/footer-links.php'; ?>
     </body>
 </html>
